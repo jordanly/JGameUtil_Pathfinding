730a38631f20b4a1274dff86eab9542cb2b1242c
Jordan ly <jordan.ly8@gmail.com>
2014-04-20 22:35:28 -0400

    Increased heuristic constant

--- src/main/java/jgpathfinder/JGPathfinderHeuristic.java
+++ src/main/java/jgpathfinder/JGPathfinderHeuristic.java
@@ -20,7 +20,7 @@ public class JGPathfinderHeuristic implements JGPathfinderHeuristicInterface {
         double dx = Math.abs(source.x - target.x);
         double dy = Math.abs(source.y - target.y);
 
-        final double D = 1;
+        final double D = 5;
 
         return D * (dx + dy);
     }
